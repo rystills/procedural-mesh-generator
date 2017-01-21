5651dc79b68fac011ed5639f5bec340bb61b4dea
Ryan Stillings <rystills@gmail.com>
2017-01-21 02:19:55 -0500

    created working propogateQuad function
    created propogateQuad function which creates a single quad of verts,
    tris, and UVs, using the new lists (this function is much less dependent
    on the current state of these lists than the old mesh generation
    functions, meaning it should be much easier to extend without adding a
    lot of complexity)

--- Assets/scripts/GenerateMesh.cs
+++ Assets/scripts/GenerateMesh.cs
@@ -11,6 +11,11 @@ public class GenerateMesh : MonoBehaviour {
 
 
     void Start() {
+        //init mesh lists
+        newVertices = new List<Vector3>();
+        newTrianglePoints = new List<int>();
+        newUVs = new List<Vector2>();
+        //build debug texture as a fallback if no material is supplied
         debugTex = new Texture2D(2, 2, TextureFormat.ARGB32, false);
         debugTex.SetPixel(0, 0, Color.red);
         debugTex.SetPixel(1, 0, Color.green);
@@ -19,11 +24,11 @@ public class GenerateMesh : MonoBehaviour {
         debugTex.wrapMode = TextureWrapMode.Repeat;
         debugTex.Apply();
         //generatePipe("normal",1);
-        generateMesh("normal", 2,2);
+        generateMesh("normal", 1,1);
 	}
 
     //construct an extruded, closed surface, with shape depending on input mode (n = number of pieces to split the pipe into)
-    void generatePipe(string mode, int n) {
+    /*void generatePipe(string mode, int n) {
         initNewMesh(n,n, 4);
 
         buildVerts(n,n, 1, .1f, false);
@@ -43,33 +48,68 @@ public class GenerateMesh : MonoBehaviour {
         buildUVs(n, false, newUVs.Length / 4, 3 * ((n + 1) * (n + 1)));
 
         finalizeMesh();
-    }
+    }*/
 
-    // construct a flat mxn rectangular mesh (n = number of pieces to split the mesh into)
+    // construct a flat mxn rectangular mesh (m = length segs, n = width segs)
     void generateMesh(string mode, int m, int n = 0) {
         n = (n == 0 ? m : n);
-        initNewMesh(m,n);
+        //initNewMesh(m,n);
         if (mode == "normal") {
-            buildVerts(m,n);
+            float xPos = 0;
+            float quadSize = 1;
+            for (int i = 0; i < m; ++i) {
+                float yPos = 0;
+                for (int r = 0; r < n; ++r) {
+                    propogateQuad(xPos,yPos, quadSize);
+                    yPos += quadSize;
+                }
+                xPos++;
+            }
         }
-        else if (mode == "wavy") {
+        /*else if (mode == "wavy") {
             buildVertsWavy(n);
         }
         buildTris(n,m);
-        buildUVs(Mathf.Max(m,n));
+        buildUVs(Mathf.Max(m,n));*/
         finalizeMesh();
     }
 
+    void propogateQuad(float xPos, float yPos, float quadSize) {
+        //step 1: generate the necessary verts and corresponding UVs
+        //case 1: there are no verts currently, so generate our first 2 side verts
+        if (newVertices.Count == 0) {
+            newVertices.Add(new Vector3(xPos, yPos, 0));
+            newUVs.Add(new Vector2(xPos, yPos));
+            newVertices.Add(new Vector3(xPos, yPos + quadSize, 0));
+            newUVs.Add(new Vector2(xPos, yPos + quadSize));
+        }
+        //case 2: we have our 2 side verts; add two more side verts and generate our tris and UVs to match
+        newVertices.Add(new Vector3(xPos + quadSize, yPos, 0));
+        newUVs.Add(new Vector2(xPos + quadSize, yPos));
+        newVertices.Add(new Vector3(xPos + quadSize, yPos + quadSize, 0));
+        newUVs.Add(new Vector2(xPos + quadSize, yPos + quadSize));
+
+        //step 2: generate the necessary tris (because this method adds a single quad, we need two new triangles, or 6 points in our list of tris)
+        //first new tri
+        newTrianglePoints.Add(newVertices.Count - 4);
+        newTrianglePoints.Add(newVertices.Count - 3);
+        newTrianglePoints.Add(newVertices.Count - 2);
+        //second new tri
+        newTrianglePoints.Add(newVertices.Count - 3);
+        newTrianglePoints.Add(newVertices.Count - 1);
+        newTrianglePoints.Add(newVertices.Count - 2);
+    }
+
     //initialize a new procedural mesh (n = number of quads)
-    void initNewMesh(int m,int n=0, int factor = 1) {
+    /*void initNewMesh(int m,int n=0, int factor = 1) {
         n = (n == 0 ? m : n);
         newVertices = new Vector3[factor * ((n + 1) * (m + 1))]; //one square mesh has 4 vertices, and each subsequent triange adds one new vert (therefore two per additional quad)
         newTrianglePoints = new int[factor * (6 * n * m)]; //n^2 total quads. each quad is 2 tris. multiply by 3 as each tri is described by 3 points
         newUVs = new Vector2[newVertices.Length]; //number of UVs should match number of vertices for proper mapping
-    }
+    }*/
 
     //construct vertices (n = number of quads)
-    void buildVerts(int m, int n, float xChange = .55f, float yChange = .55f, bool orientUp = false, int startOffset = 0, float xOffset = 0, float yOffset = 0, float zOffset = 0) {
+    /*void buildVerts(int m, int n, float xChange = .55f, float yChange = .55f, bool orientUp = false, int startOffset = 0, float xOffset = 0, float yOffset = 0, float zOffset = 0) {
         for (int x = 0, listPos = startOffset; x < m + 1; x++) {
             for (int y = 0; y < n + 1; y++, listPos++) {
                 newVertices[listPos] = new Vector3(xOffset + x * xChange, yOffset + y * yChange * (orientUp ? 1 : 0), zOffset + y * yChange * (orientUp ? 0 : 1));
@@ -121,16 +161,16 @@ public class GenerateMesh : MonoBehaviour {
     void buildUVs(float factor, bool orientUp = false, int n = 0, int startOffset = 0) {
         n = (n == 0 ? newUVs.Length : n);
         for (int i = startOffset; i < startOffset + n; newUVs[i] = new Vector2(newVertices[i].x / factor, (orientUp ? newVertices[i].y : newVertices[i].z) / factor), i++) ;
-    }
+    }*/
 
     //construct the new mesh, and attach the appropriate components
     void finalizeMesh() {
         Mesh mesh = new Mesh();
         MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
         meshFilter.mesh = mesh;
-        mesh.vertices = newVertices;
-        mesh.triangles = newTrianglePoints;
-        mesh.uv = newUVs;
+        mesh.vertices = newVertices.ToArray();
+        mesh.triangles = newTrianglePoints.ToArray();
+        mesh.uv = newUVs.ToArray();
         meshFilter.mesh.RecalculateBounds();
         meshFilter.mesh.RecalculateNormals();
         MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
