b90d6da42e36fd1993cb54f06285e21174191268
Ryan Stillings <rystills@gmail.com>
2017-02-08 01:39:34 -0500

    Added addVert, addTriangleIndex to VertexDict
    Added addVert function to VertexDict to complement getVert, along with
    the removal of 'normalsIndex' from VertexData, as the normals index must
    always be the same as the vertices index, which is further the same as
    the UVs index. Added addTriangleIndex function to VertexData, which
    should later be used to maintain trianglesIndices.

--- Assets/scripts/VertexDict.cs
+++ Assets/scripts/VertexDict.cs
@@ -14,15 +14,32 @@ public class VertexDict {
 		verts = new Dictionary<Vector3, Dictionary<Quaternion, VertexData>>();
 	}
 
+	//return Vector3 vec rounded to smoothnessFloatDigits
+	public Vector3 roundVector(ref Vector3 vec) {
+		return new Vector3((float)Math.Round((double)vec.x, smoothnessFloatDigits), (float)Math.Round((double)vec.y, smoothnessFloatDigits),
+		   (float)Math.Round((double)vec.z, smoothnessFloatDigits));
+	}
+
+	//get all vertices at position pos, regardless of normal dir
 	public VertexData[] getVerts(Vector3 pos) {
 		return verts[pos].Values.ToArray();
+	}
+
+	//add a vert at position pos with normal normal
+	public VertexData addVert(int verticesIndex, Vector3 pos, Vector3 normal, List<int> trianglesIndices = null) {
+		Vector3 roundedPos = roundVector(ref pos);
+		if (!verts.ContainsKey(roundedPos)) {
+			verts[roundedPos] = new Dictionary<Quaternion, VertexData>();
+		}
+		VertexData newVert = new VertexData(verticesIndex, trianglesIndices);
+		verts[roundedPos][Quaternion.Euler(normal.x, normal.y, normal.z)] = newVert;
+		return newVert;
 	}
 
 	//get vertex at position pos with normal normal
 	public VertexData getVert(Vector3 pos, Vector3 normal) {
 		//check if rounded position is a valid key first
-		Vector3 roundedPos = new Vector3((float)Math.Round((double)pos.x, smoothnessFloatDigits), (float)Math.Round((double)pos.y, smoothnessFloatDigits), 
-			(float)Math.Round((double)pos.z, smoothnessFloatDigits));
+		Vector3 roundedPos = roundVector(ref pos);
 		Dictionary<Quaternion, VertexData> quatKey = null;
 		if (verts.ContainsKey(roundedPos)) {
 			quatKey = verts[roundedPos];
@@ -46,8 +63,8 @@ public class VertexDict {
 		if (quatKey == null) { //no vector could be found at this position
 			return null;
 		}
-		//found a valid position; search for the closest rotation at this position that falls within smoothnessFloatTolerance
-		
+
+		//found a valid position; search for the closest normal at this position that falls within smoothnessFloatTolerance
 		bool foundCandidate = false;
 		Quaternion smallestKey = Quaternion.identity;
 		float smallestDiff = smoothnessFloatTolerance;
@@ -62,18 +79,24 @@ public class VertexDict {
 		if (foundCandidate) {
 			return quatKey[smallestKey];
 		}
-		return null; //a vector was found at this position, but no matching direction within tolerance was found at that position
+		return null; //a vector was found at this position, but no matching normal within tolerance was found at that position
 	}
 }
 
 public class VertexData {
-	public int verticesIndex;
-	public int normalsIndex;
-	public List<int> trianglesIndex;
+	public int verticesIndex; //can be used to lookup vertex, UV, or normal
+	public List<int> trianglesIndices; //to be used as a list of all instances of this Vertex's index in mesh's triangle list
 
-	public VertexData(int verticesIndex, int normalsIndex, List<int> trianglesIndex) {
+	public VertexData(int verticesIndex, List<int> trianglesIndices = null) {
+		if (trianglesIndices == null) {
+			trianglesIndices = new List<int>();
+		}
 		this.verticesIndex = verticesIndex;
-		this.normalsIndex = normalsIndex;
-		this.trianglesIndex = trianglesIndex;
+		this.trianglesIndices = trianglesIndices;
 	}	
+
+	//add another index to trianglesIndices
+	public void adadTriangleIndex(int index) {
+		this.trianglesIndices.Add(index);
+	}
 }
