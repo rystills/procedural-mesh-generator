3e2d2e87f1ec4036056c8d758b9a0da86bd628d9
Ryan Stillings <rystills@gmail.com>
2017-02-16 05:22:38 -0500

    MeshEffects parameters now editable from editor
    Parameters for MeshEffects are now declared in the editor, in much the
    same way as MeshShapes parameters

--- Assets/scripts/MeshEffects.cs
+++ Assets/scripts/MeshEffects.cs
@@ -6,30 +6,35 @@ using System.Linq;
 public class MeshEffects : MonoBehaviour {
 
 	public string animMode;
+	public List<string> args;
 	GenerateMesh meshGenerator;
 
 	// Use this for initialization
 	void Start () {
 		meshGenerator = this.GetComponent<GenerateMesh>();
 	}
+
+	//wave vert groups by the first vert's normal for the sake of simplicity
+	void animateWave(float speed, float numWaves, float amplitude) {
+		int count = 0; //only increase count after each vert group, rather than each individual vert
+		foreach (Dictionary<Quaternion, VertexData> dict in meshGenerator.vertDict.verts.Values) {
+			VertexData[] curVerts = dict.Values.ToArray();
+			for (int i = 0; i < curVerts.Length; ++i) {
+				meshGenerator.vertices[curVerts[i].verticesIndex] = meshGenerator.vertices[curVerts[i].verticesIndex] + meshGenerator.normals[curVerts[0].verticesIndex].normalized *
+				(Mathf.Sin(speed * Time.time + (numWaves * 6.28f / (float)meshGenerator.vertDict.verts.Values.Count) * count) / (2000 / amplitude));
+			}
+			++count;
+		}	
+}
 	
 	// Update is called once per frame
 	void Update () {
 		if (animMode == "wave") {
-			//wave vert groups by the first vert's normal for the sake of simplicity
-			int count = 0;
-			foreach (Dictionary<Quaternion, VertexData> dict in meshGenerator.vertDict.verts.Values) {
-				VertexData[] curVerts = dict.Values.ToArray();
-				for (int i = 0; i < curVerts.Length; ++i) {
-						meshGenerator.vertices[curVerts[i].verticesIndex] = meshGenerator.vertices[curVerts[i].verticesIndex] + meshGenerator.normals[curVerts[0].verticesIndex].normalized * 
-						(Mathf.Sin(2 * Time.time + (6f*6.28f / (float)meshGenerator.vertDict.verts.Values.Count)* count) / 2000);
-				}
-				++count;
-			}
-			Mesh mesh = GetComponent<MeshFilter>().mesh;
-			mesh.vertices = meshGenerator.vertices.ToArray();
-			this.GetComponent<MeshCollider>().sharedMesh = mesh;
+			animateWave(float.Parse(args[0]), float.Parse(args[1]), float.Parse(args[2]));
 		}
-	
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.vertices = meshGenerator.vertices.ToArray();
+		this.GetComponent<MeshCollider>().sharedMesh = mesh;
+
 	}
 }
